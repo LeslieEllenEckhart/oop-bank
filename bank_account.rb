379ed2ad6a1b462bd763f05d8ad2cde405d7fa5d
Leslie Eckhart <leslie.ellen.eckhart@gmail.com>
2017-10-11 22:57:04 +0000

    Update after it #2

--- bank_account.rb
+++ bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
-
+    attr_reader :balance
+    
+    def initialize (opening_balance, account_holder)
+        @balance = opening_balance
+    end
+    
+#     def deposit (deposit_amt, account_holder)
+#         @deposit_amt = deposit_amt
+#         @account_holder = account_holder
+#         @balance = @balance + deposit_amt
+#     end
+#    def balance
+#    end
 end
+
